0fa7874d7523ca11ddc2f336430bdc79efedd0a1
Matteo Fusilli <matteo.fusilli@adarga.ai>
2020-02-06 12:12:26 +0000

    added function to write .txt file

--- main.go
+++ main.go
@@ -4,21 +4,60 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"net/http"
 	"encoding/json"
 	"./config"
 )
+
+/* 
+* Get headline by recency and source (e.g Google maps)
+*	Get the top articles from targeted topics, at least 2 topics per categpory.
+*	Write a .txt file with only the content from those topics
+* 1 function to fetch the content and 1 function to write it
+*/
+
+// The final content
+var c []string;
 
 type Response struct {
 	Article []Article `json:"articles"`
 }
 
 type Article struct {
+	Description string `json:"description"`
 	Content string `json:"content"`
 }
 
+func generateTrainData(c []string){
+	f, err := os.Create("train_data.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := 0; i < len(c); i++ {
+		l, err := f.WriteString(c[i])
+		if err != nil {
+			fmt.Println(err)
+			f.Close()
+			return
+		}
+		fmt.Println(l, "Bytes written successfully")
+		err = f.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
 
 func main() {
+	
+	keyWords := []string{"bitcoin", "coronavirus", "ukraine"}
+	for i := 0; i < len(keyWords); i++ {
+		fmt.Println("My jkeyword: ", keyWords[i])
+	}
 
 	resp, err := http.Get(config.TopHeadlines)
 	if err != nil {
@@ -36,13 +75,13 @@ func main() {
 	var responseObject Response
 	json.Unmarshal(responseData, &responseObject)
 
-	fmt.Println(responseObject.Article)
-
 	for i := 0; i < len(responseObject.Article); i++ {
-		fmt.Println(responseObject.Article[i].Content)
+		// fmt.Println(responseObject.Article[i].Content)
+		c = append(c, responseObject.Article[i].Content)
+		fmt.Println(c)
 	}
 
-	// log.Println(string(body))
+	generateTrainData(c)
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":8080", nil)
