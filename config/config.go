553db9098c5be3bb460faa36edda0c3836c63a57
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2020-02-17 21:16:28 +0000

    loop through topics to aggregate news

--- config/config.go
+++
@@ -1,5 +0,0 @@
-package config
-
-var API_KEY = "91d30a50507549e8a1134c0642992de4"
-var TopHeadlines = "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=" + API_KEY
-var ByTopic = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=" + API_KEY
--- main.go
+++ main.go
@@ -1,49 +1,70 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
-	"./config"
+	"os"
 )
+
+/*
+* The news aggregator needs to scrape information for the following entity types
+MEDICAL
+BIOLOGICAL
+WEAPONS
+TECHNOLOGY
+*/
+
+var API_KEY = "91d30a50507549e8a1134c0642992de4"
 
 type Response struct {
 	Article []Article `json:"articles"`
 }
 
 type Article struct {
+	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
-
 func main() {
 
-	resp, err := http.Get(config.TopHeadlines)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-
-	responseData, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatalln(err)
-	}	
-
 	var responseObject Response
-	json.Unmarshal(responseData, &responseObject)
 
-	fmt.Println(responseObject.Article)
+	topics := []string{"china", "iraq", "bitcoin", "coronavirus", "ukraine", "iphone", "google", "syria", "virus", "global warming", "environment"}
 
-	for i := 0; i < len(responseObject.Article); i++ {
-		fmt.Println(responseObject.Article[i].Content)
+	f, err := os.Create("train.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	// log.Println(string(body))
+	for i := 0; i < len(topics); i++ {
+		resp, err := http.Get("https://newsapi.org/v2/everything?q=" + topics[i] + "&apiKey=" + API_KEY)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.ListenAndServe(":8080", nil)
+		defer resp.Body.Close()
+
+		responseData, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		json.Unmarshal(responseData, &responseObject)
+
+		for i := 0; i < len(responseObject.Article); i++ {
+			l, err := f.WriteString(responseObject.Article[i].Title + "\n" + responseObject.Article[i].Content)
+			if err != nil {
+				fmt.Println(err)
+				f.Close()
+				return
+			}
+			fmt.Println(l, "bytes written successfully")
+		}
+	}
+	/* http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":8080", nil) */
 }
