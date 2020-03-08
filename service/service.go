70be2efafe24783b84ecb4e3e30e32df8c68a897
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2020-03-08 12:41:05 +0000

    fetch by topic

--- service/service.go
+++ service/service.go
@@ -33,27 +33,27 @@ type New struct {
 	Author string
 }
 
-func Router() *mux.Router {
-
-	router := mux.NewRouter()
-	router.HandleFunc("/", index)
-	router.HandleFunc("/api/topic", getTopic).Methods("POST", "OPTIONS")
-	return router
-}
-
 var tpl *template.Template
 var endpoint = "http://newsapi.org/v2/top-headlines?sources=google-news&apiKey=API_KEY"
 var apiKey = goDotEnvVariable("API_KEY")
+
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", index).Methods("GET", "POST", "OPTIONS")
+	router.HandleFunc("/topic", byTopic).Methods("GET", "POST", "OPTIONS")
+	return router
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
 
 func handleCors(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "*")
 	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 	(*w).Header().Set("Content-Type", "*")
-}
-
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func goDotEnvVariable(key string) string {
@@ -102,37 +102,71 @@ func fetchNews() []New {
 	return news
 }
 
-func index(res http.ResponseWriter, req *http.Request) {
+func fetchByTopic(t string) []New {
+	var responseObject Response
 
-	news := fetchNews()
-
-	err := tpl.ExecuteTemplate(res, "index.gohtml", news)
+	resp, err := http.Get("http://newsapi.org/v2/everything?q=" + t + "&apiKey=" + apiKey)
 	if err != nil {
-		log.Fatalln("template didn't execute: ", err)
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	responseData, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	json.Unmarshal(responseData, &responseObject)
+
+	news := make([]New, 0)
+
+	for i := 0; i < len(responseObject.Article); i++ {
+		new := New{
+			responseObject.Article[i].Title,
+			responseObject.Article[i].Content,
+			responseObject.Article[i].Image,
+			responseObject.Article[i].URL,
+			responseObject.Article[i].Author,
+		}
+		news = append(news, new)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	return news
+}
+
+func byTopic(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Println("GET request in action")
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			r.ParseForm()
+		}
+		topic := r.FormValue("topic")
+		news := fetchByTopic(topic)
+		err := tpl.ExecuteTemplate(w, "bytopic.gohtml", news)
+		if err != nil {
+			log.Fatalln("template didn't execute: ", err)
+		}
+	default:
+		fmt.Fprintf(w, "Request method not supported.")
 	}
 }
 
-// GetTopic : get the user input on news topic
-func getTopic(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
+
+	news := fetchNews()
 	handleCors(&w, r)
 
 	if (*r).Method == "OPTIONS" {
 		return
 	}
-	switch r.Method {
-	case "GET":
-		fmt.Fprintf(w, "Request method not supported.")
-	case "POST":
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
-		}
 
-		fmt.Fprintf(w, "r.PostForm = %v\n", r.PostForm)
-		topic := r.FormValue("topic")
-		fmt.Fprintf(w, "Topic = %s\n", topic)
-
-	default:
-		fmt.Fprintf(w, "Request method not supported.")
+	err := tpl.ExecuteTemplate(w, "index.gohtml", news)
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
 	}
 }
